07febdea8e66a64d79b71da1a58c72189851d2f3
192.168.0.1 <90830312+evilbitmap@users.noreply.github.com>
2021-10-01 09:45:18 +0200

    Update app.py

--- working with data/app.py
+++ working with data/app.py
@@ -1,34 +1,41 @@
-from printdata import PrintSpecificClassWithNumber
 from printdata import PrintSpecificClass
-from printdata import PrintData
 from printdata import PrintAllClasses
-from tridy import tridy
+from printdata import PrintNameOfClasses
 
 
 def menu():
-    print("1: print all classes")
-    print("2: print specific class")
-    choice=int(input(": "))
+    print("_________________________")
+    print("1-> print all classes")
+    print("2-> print specific class")
+    print("_________________________")
+    print("3-> exit")
+    print("_________________________")
+    choice=int(input(">"))
     match(choice):
         case 1:
-            PrintData()
+            PrintAllClasses()
         case 2:
-            print("choose class...")
-            for x in tridy:
-                PrintSpecificClassWithNumber()
-                choiceSpecificClass=int(input(": "))
-                if choiceSpecificClass in tridy[choiceSpecificClass]["class"]:
+            while True:
+                print("_________________________")
+                print("choose a class...")
+                PrintNameOfClasses()
+                print("_________________________")
+                print("00-> back")
+                print("_________________________")
+                choiceSpecificClass=str(input(">"))
+                if choiceSpecificClass == ("00"):
+                    return
+                else:
                     PrintSpecificClass(choiceSpecificClass)
-
-
-
-
-
+        case 3:
+            exit()
+        case _:
+            print("please choose a number")
 
 def main():
-    print("menu \n")
+    menu()
 
 
 if __name__ == "__main__":
-    main()
-    menu()
+    while True:
+        main()
