dc5ab0f5352b1c068d111a6af5258fa34708c91c
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2021-10-15 10:26:16 +0200

    Update dice.py
    Procentage seems to be broken

--- games/dice.py
+++ games/dice.py
@@ -7,6 +7,9 @@ randomNumberUser = 0
 randomNumberPC = 0
 userPoints = 0
 computerPoints = 0
+gamesPlayed = 0
+userProcentage = 0
+computerProcentage = 0
 
 #command pro vymazání console
 def cls():
@@ -15,6 +18,15 @@ def cls():
 #příkáz pro čekání na jakejkoliv input key
 def wait():
     msvcrt.getch()
+
+def savefile():
+    global userPoints
+    global computerPoints
+    global userProcentage
+    global computerProcentage
+    f = open("score (dice.py).txt", "w")
+    f.write(f"your score: {userPoints} ; computer score: {computerPoints} \n {userProcentage} ; {computerProcentage} \n games played: {gamesPlayed}")
+    f.close
 
 #vygeneruje náhodný číslo
 def generateRandomNum():
@@ -34,6 +46,8 @@ def main():
     print("--------------------------------")
     print(f"Your dice: {randomNumberUser} ; PC dice: {randomNumberPC}")
     print("--------------------------------")
+    print(f"games played: {gamesPlayed}")
+    print("--------------------------------")
     print("press enter to roll!")
     print("--------------------------------")
     print("--------------------------------")
@@ -48,20 +62,42 @@ def main():
         generateRandomNum()
         ifLowerOrHigher()
 
+def calculateProcentage():
+    global userPoints
+    global computerPoints
+    global gamesPlayed
+    global userProcentage
+    global computerProcentage
+
+    userProcentage = userPoints / gamesPlayed
+    userProcentage = userProcentage * 100
+    computerProcentage = computerPoints / gamesPlayed
+    computerProcentage = computerProcentage * 100
+
+
+
 
 #určuje kdo vyhrál
 def ifLowerOrHigher():
     global userPoints
     global computerPoints
+    global gamesPlayed
+
     if(randomNumberUser > randomNumberPC):
         userPoints += 1
+        gamesPlayed += 1
+        calculateProcentage()
         main()
 
     elif(randomNumberUser < randomNumberPC):
         computerPoints += 1
+        gamesPlayed += 1
+        calculateProcentage()
         main()
 
     elif(randomNumberUser == randomNumberPC):
+        gamesPlayed += 1
+        calculateProcentage()
         main()
 
 #uživatelské příkazy
@@ -70,15 +106,21 @@ def commands():
     global computerPoints
     cls()
     print("commands:")
-    print("         reset ; exit")
+    print("         reset ; exit ; save score")
     print("type back to go back")
     userinput = input(">")
     if(userinput == "reset"):
         userPoints = 0
         computerPoints = 0
         main()
+    
+    elif(userinput == "save score"):
+        savefile()
+        main()
+
     elif(userinput == "exit"):
         exit()
+    
 
 
 if (__name__ == "__main__"):
