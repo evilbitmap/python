740493c3097366262562731fa6b3e2e65eda13ee
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2021-11-04 10:56:54 +0100

    Update NSD.py

--- algorithms/NSD.py
+++ algorithms/NSD.py
@@ -4,10 +4,17 @@ def main():
     w=int(input("number two:"))
 
     while(w != 0):
+        #"r" vyjde zbytek
         r = u % w
+
+        #"u" se bude = w což je to větší číslo
         u = w
+
+        #a "w" se bude rovnat ten zbytek a pokud to bude 0 tak print "u"
         w = r
+
     print(u)
+    print("...")
 
 
 
