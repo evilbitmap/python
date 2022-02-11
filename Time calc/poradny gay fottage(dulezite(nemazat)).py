cc8133eb26fbcf0c9ef540c81a047c2aca893a42
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2022-02-11 10:37:46 +0100

    Create poradny gay fottage(dulezite(nemazat)).py

---
+++ Time calc/poradny gay fottage(dulezite(nemazat)).py
@@ -0,0 +1,7 @@
+def převést():
+    if(Sec.get() > 59):
+        sekunda = Sec.get() % 60
+        minuta = (Sec.get() - sekunda) / 60
+
+        print("min" + str(minuta))
+        print("sec" + str(sekunda))
