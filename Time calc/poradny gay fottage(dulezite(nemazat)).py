7b93a07d6c73846fd05f0166386a0d792d3b30a7
192.168.0.1 <90830312+negativexp@users.noreply.github.com>
2022-02-12 12:08:05 +0100

    Delete poradny gay fottage(dulezite(nemazat)).py

--- Time calc/poradny gay fottage(dulezite(nemazat)).py
+++
@@ -1,9 +0,0 @@
-Sec.get = textbox Sekunda
-
-def převést():
-    if(Sec.get() > 59):
-        sekunda = Sec.get() % 60
-        minuta = (Sec.get() - sekunda) / 60
-
-        print("min" + str(minuta))
-        print("sec" + str(sekunda))
